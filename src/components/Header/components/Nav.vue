bdf7fc6acb17f45ac612d0c3decf12ffe68d5439
huanshenyi <txy1226052@gmail.com>
2020-01-08 00:32:10 +0900

    nav item の紹介デザインの調整

--- src/components/Header/components/Nav.vue
+++ src/components/Header/components/Nav.vue
@@ -2,6 +2,19 @@
     <div class="nav">
       <div class="nav-item">
           <span>時給計算</span>
+          <div class="children">
+              <div class="product">
+                  <a href="" target="_blank">
+                      <div class="pro-img">
+                          <img src="../../../assets/dentaku.png" alt="">
+                      </div>
+                      <div class="column">
+                            <div class="pro-name">給料計算</div>
+                            <div class="pro-content">紹介文紹介文紹介文紹介文</div>
+                      </div>
+                  </a>
+              </div>
+          </div>
       </div>
       <div class="nav-item">
           <span>何か</span>
@@ -33,6 +46,52 @@ export default class nav extends Vue{
        color: #333;
        line-height: 56px;
        margin-right: 20px;
+       &:hover{
+           color: #ec6149;
+           .children{
+               height: 120px;
+           }
+       }
+       .children{
+           background-color: white;
+           border-top: 1px solid #f0f0f0;
+           position: absolute;
+           top: 56px;
+           left: 0;
+           width: 1226px;
+           box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
+           z-index: 10;
+           height: 120px;
+           .product{
+               display: inline-block;
+               width: 70%;
+               height: 120px;
+               text-align: center;
+               a{
+                   display: inline-block;
+               }
+               img{
+                   width: auto;
+                   height: 120px;
+                   padding-right: 30px;
+               }
+               .pro-img{
+                   float: left;
+                   height: 100px;
+               }
+               .column{
+                   display: flex;
+                   flex-direction:column;
+                   .pro-name{
+                        float: left;
+                        height: 25px;
+                   }
+                   .pro-content{
+                        float: left;
+                   }
+               }
+           }
+       }
   }
 }
 </style>
